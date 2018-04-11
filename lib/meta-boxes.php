ce823829cc9cbc7589b7b6f72fd4c76966c4fe3f
mikeyrayvon <mikey@interglobal.vision>
2018-04-11 17:41:28 -0500

    add meta fields

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -100,13 +100,25 @@ function igv_cmb_metaboxes() {
 		'name' => esc_html__( 'Image', 'cmb2' ),
 		'id'   => 'image',
 		'type' => 'file',
+    'options' => array(
+  		'url' => false, // Hide the text input for the url
+  	),
 	) );
 
   $exhibition_metabox->add_group_field( $exhibition_images_group, array(
 		'name' => esc_html__( 'Caption', 'cmb2' ),
 		'id'   => 'caption',
 		'type' => 'wysiwyg',
+    'options' => array(
+	    'wpautop' => false, // use wpautop?
+	    'media_buttons' => false, // show insert/upload button(s)
+	    'textarea_rows' => 1, // rows="..."
+	    'teeny' => true, // output the minimal editor config used in Press This
+	    'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+  	),
 	) );
+
+  // LOCATION
 
   $location_metabox = new_cmb2_box( array(
 		'id'               => $prefix . 'location_metabox',
@@ -120,6 +132,76 @@ function igv_cmb_metaboxes() {
 		'id'      => $prefix . 'location_city',
 		'type'    => 'text',
 	) );
+
+  // ARTIST
+
+  $artist_metabox = new_cmb2_box( array(
+ 		'id'            => $prefix . 'artist_metabox',
+ 		'title'         => esc_html__( 'Details', 'cmb2' ),
+ 		'object_types'  => array( 'artist' ), // Post type
+ 	) );
+
+  $artist_metabox->add_field( array(
+		'name' => esc_html__( 'CV PDF', 'cmb2' ),
+		'id'   => $prefix . 'artist_cv_pdf',
+		'type' => 'file',
+    'options' => array(
+  		'url' => false, // Hide the text input for the url
+  	),
+    'text'    => array(
+  		'add_upload_file_text' => 'Add PDF'
+  	),
+  	// query_args are passed to wp.media's library query.
+  	'query_args' => array(
+  		'type' => 'application/pdf', // Make library only display PDFs.
+  	),
+	) );
+
+  $artist_metabox->add_field( array(
+		'name' => esc_html__( 'Cute studio pic', 'cmb2' ),
+		'id'   => $prefix . 'artist_studio_pic',
+		'type' => 'file',
+    'options' => array(
+  		'url' => false, // Hide the text input for the url
+  	),
+    'text'    => array(
+  		'add_upload_file_text' => 'Add Image'
+  	),
+	) );
+
+  $artist_images_group = $artist_metabox->add_field( array(
+		'id'          => $prefix . 'artist_images',
+		'type'        => 'group',
+		'options'     => array(
+			'group_title'   => esc_html__( 'Image {#}', 'cmb2' ), // {#} gets replaced by row number
+			'add_button'    => esc_html__( 'Add Another Image', 'cmb2' ),
+			'remove_button' => esc_html__( 'Remove Image', 'cmb2' ),
+			'sortable'      => true, // beta
+		),
+	) );
+
+  $artist_metabox->add_group_field( $artist_images_group, array(
+		'name' => esc_html__( 'Image', 'cmb2' ),
+		'id'   => 'image',
+		'type' => 'file',
+    'options' => array(
+  		'url' => false, // Hide the text input for the url
+  	),
+	) );
+
+  $artist_metabox->add_group_field( $artist_images_group, array(
+		'name' => esc_html__( 'Caption', 'cmb2' ),
+		'id'   => 'caption',
+		'type' => 'wysiwyg',
+    'options' => array(
+	    'wpautop' => false, // use wpautop?
+	    'media_buttons' => false, // show insert/upload button(s)
+	    'textarea_rows' => 1, // rows="..."
+	    'teeny' => true, // output the minimal editor config used in Press This
+	    'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+  	),
+	) );
+
 
 }
 ?>
