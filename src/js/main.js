fbc050f139b9b045c3a149aa47b4116c13d9501e
mikeyrayvon <mikey@interglobal.vision>
2018-04-11 20:27:00 -0500

    add postit positioning

--- src/js/main.js
+++ src/js/main.js
@@ -18,12 +18,24 @@ class Site {
   }
 
   onResize() {
+    this.$windowHeight = $(window).height();
+    this.$windowWidth = $(window).width();
 
+    this.dotSize();
+    this.postitDot();
   }
 
   onReady() {
     lazySizes.init();
 
+    this.windowHeight = $(window).height();
+    this.windowWidth = $(window).width();
+    this.dotDiameter = 15;
+
+    this.$postit = $('#postit');
+
+    this.dotSize();
+    this.positionPostit();
   }
 
   fixWidows() {
@@ -33,6 +45,60 @@ class Site {
       string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
       $(this).html(string);
     });
+  }
+
+  dotSize() {
+    var $dot = $('svg path.logo-dot');
+    var _this = this;
+
+    if ($dot.length) {
+      _this.dotDiameter = $dot.first().width();
+    }
+
+    $('.dot').css({
+      'height': _this.dotDiameter,
+      'width': _this.dotDiameter,
+      'border-radius': _this.dotDiameter,
+    });
+
+    this.stylePostitDot();
+  }
+
+  positionPostit() {
+    var _this = this;
+
+    if (_this.$postit.length) {
+      var imageHeight = _this.$postit.outerHeight();
+      var imageWidth = _this.$postit.outerWidth();
+
+      var maxTop = 100 - ((imageHeight / _this.windowHeight) * 100);
+      var maxLeft = 100 - ((imageWidth / _this.windowWidth) * 100);
+
+      var randomTop = _this.randomInt(maxTop);
+      var randomLeft = _this.randomInt(maxLeft);
+
+      var randomRotate = _this.randomInt(10, -10);
+
+      _this.$postit.css({
+        'top': randomTop + 'vh',
+        'left': randomLeft + 'vw',
+        'transform': 'rotate(' + randomRotate + 'deg)',
+      });
+    }
+  }
+
+  stylePostitDot() {
+    var _this = this;
+
+    if (_this.$postit.length) {
+      $('#postit-dot').css({
+        'top': '-' + (_this.dotDiameter / 2) + 'px',
+      });
+    }
+  }
+
+  randomInt(max, min = 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
 
