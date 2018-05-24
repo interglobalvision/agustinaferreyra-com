7b95688f5dec6713a193aedab4ad3ec11bf2a248
mikeyrayvon <mikey@interglobal.vision>
2018-05-24 15:29:49 -0500

    add padding

--- 404.php
+++ 404.php
@@ -9,7 +9,7 @@ get_header();
         <div id="logo-holder" class="grid-item flex-grow">
           <?php get_template_part('partials/logo'); ?>
         </div>
-        <div class="grid-item item-s-12 item-m-6">
+        <div class="grid-item item-s-12 item-m-6 padding-bottom-mid">
           <h1 class="font-size-large font-heavy">404?!</h1>
           <p>Whatever you're lookin' for ain't here</p>
         </div>
