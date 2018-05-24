94412342e85407b0b4c80f699a091e47df6a085f
mikeyrayvon <mikey@interglobal.vision>
2018-05-24 15:38:31 -0500

    use text-max-width on page content

--- page.php
+++ page.php
@@ -14,7 +14,7 @@ if (have_posts()) {
   while (have_posts()) {
     the_post();
 ?>
-        <article id="page-<?php the_ID(); ?>" <?php post_class('grid-item item-s-12 item-m-6 margin-bottom-mid'); ?>>
+        <article id="page-<?php the_ID(); ?>" <?php post_class('grid-item item-s-12 item-m-6 margin-bottom-mid text-max-width'); ?>>
           <h1 class="font-size-large font-heavy margin-bottom-mid"><?php the_title(); ?></h1>
           <?php the_content(); ?>
         </article>
