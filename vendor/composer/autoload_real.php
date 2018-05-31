380503dc16212a4d48c4868f94bfe02c637899d8
mikeyrayvon <mikey@interglobal.vision>
2018-05-30 19:22:09 -0500

    add guzzlehttp

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit4175f873da09c0c5a647da769387b889
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit4175f873da09c0c5a647da769387b889::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire4175f873da09c0c5a647da769387b889($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire4175f873da09c0c5a647da769387b889($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
