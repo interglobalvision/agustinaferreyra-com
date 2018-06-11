c01768269dc8e536c37a1d5ad19d6efc946510d6
mikeyrayvon <mikey@interglobal.vision>
2018-06-11 18:04:00 -0500

    rewrite blog fact function as class. add last expo, weather conditions, & custom strings options

--- lib/logo-facts.php
+++ lib/logo-facts.php
@@ -1,92 +1,54 @@
 <?php
-function renderLogoFact() {
-  $facts = array(
-    'temp' => 'Current temp in CDMX is %d&deg; C',
+class BlogFact {
+  public $facts = array(
+    'weather' => array(
+      'temp' => 'Current temp in CDMX is %d&deg;C',
+      'conditions' => '%s in CDMX',
+    ),
     'expo' => array(
-      'close' => 'The current expo closes in %d days',
+      'closes' => 'The current expo closes in %d days',
       'opens' => 'The next expo opens in %d days',
-    )
+      'closed' => 'The last expo closed %d days ago',
+    ),
   );
 
-  $key = array_rand($facts);
+  public function renderFact() {
+    $site_options = get_site_option('_igv_site_options');
 
-  // TODO: split expo randomly. add 'closed' for last expo 
-
-  if ($key === 'expo') {
-    // Get current or upcoming exhibition
-
-    $now = time();
-
-    $args = array(
-      'post_type' => array( 'exhibition' ),
-      'orderby' => 'meta_value_num',
-      'meta_key' => '_igv_exhibition_start',
-      'meta_query' => array(
-        'relation' => 'AND',
-        array(
-          'key'     => '_igv_exhibition_start',
-          'value'   => $now,
-          'compare' => '<',
-        ),
-        array(
-          'key'     => '_igv_exhibition_end',
-          'value'   => $now,
-          'compare' => '>',
-        ),
-      ),
-      'posts_per_page' => 1,
-      'tax_query' => array(
-        array(
-          'taxonomy' => 'location',
-          'field'    => 'slug',
-          'terms'    => 'galeria-agustina-ferreyra',
-        ),
-    	),
-    );
-
-    $expo_query = new WP_Query( $args );
-
-    if ($expo_query->have_posts()) {
-      while($expo_query->have_posts()) {
-        $expo_query->the_post();
-
-        $string = $facts[$key]['close'];
-
-        $expo_end_date = get_post_meta(get_the_ID(), '_igv_exhibition_end', true);
-
-        $value = abs($now - $expo_end_date)/60/60/24;
-
-      }
-    } else {
-      $args['meta_query'] = array(
-        array(
-          'key'     => '_igv_exhibition_start',
-          'value'   => $now,
-          'compare' => '>',
-        ),
-      );
-
-      $expo_query = new WP_Query( $args );
-
-      if ($expo_query->have_posts()) {
-        while($expo_query->have_posts()) {
-          $expo_query->the_post();
-
-          $string = $facts[$key]['opens'];
-
-          $expo_start_date = get_post_meta(get_the_ID(), '_igv_exhibition_start', true);
-
-          $value = abs($now - $expo_start_date)/60/60/24;
-
-        }
-      } else {
-        $key = 'temp';
-      }
+    if (!empty($site_options['_igv_blog_logo_strings'])) {
+      $this->facts['string'] = $site_options['_igv_blog_logo_strings'];
     }
+
+    $fact = $this->getFact();
+
+    wp_reset_query();
+
+    echo $fact;
   }
 
-  if ($key === 'temp') {
-    // Get Site Options
+  public function getFact() {
+    switch (array_rand($this->facts)) {
+      case 'weather':
+        $fact = $this->getWeatherFact();
+        break;
+      case 'expo':
+        $fact = $this->getExpoFact();
+        break;
+      case 'string':
+        $fact = $this->getStringFact();
+        break;
+      default:
+        $fact = false;
+    }
+
+    if (!$fact) {
+      $fact = $this->getFact();
+    }
+
+    return $fact;
+  }
+
+  public function getWeatherFact() {
     $site_options = get_site_option('_igv_site_options');
 
     // Check the API key
@@ -100,28 +62,190 @@ function renderLogoFact() {
 
       // Wrapped in try/catch to catch exceptions
       try {
-
-
-        // Requestes all zipcodes whithin 3 miles
+        // Requests weather data
         $res = $client->request('GET', 'http://api.openweathermap.org/data/2.5/weather?id=3530597&units=metric&appid=' . $api_key);
 
         // Decode the response's body
-        $decoded_res = json_decode($res->getBody());
+        $weatherRes = json_decode($res->getBody());
 
-        $string = $facts[$key];
-
-        $value = $decoded_res->main->temp;
-
+        switch (array_rand($this->facts['weather'])) {
+          case 'temp':
+            return $this->getWeatherTemp($weatherRes);
+            break;
+          case 'conditions':
+            return $this->getWeatherConditions($weatherRes);
+            break;
+          default:
+            return false;
+        }
       } catch (Exception $e) {
-        $value = 0;
-        $string = 'Everything<br> else';
-      }
+        unset($this->facts['weather']);
 
+        return false;
+      }
     } else {
-      $value = 0;
-      $string = 'Everything<br> else';
+      unset($this->$facts['weather']);
+      return false;
     }
   }
 
-  echo sprintf($string, $value);
+  public function getWeatherTemp($weatherRes) {
+    // Display current temperature in CDMX
+
+    $string = $this->facts['weather']['temp'];
+
+    $value = $weatherRes->main->temp;
+
+    return sprintf($string, $value);
+  }
+
+  public function getWeatherConditions($weatherRes) {
+    // Display current weather conditions in CDMX
+
+    $string = $this->facts['weather']['conditions'];
+
+    // https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+    $value = ucfirst($weatherRes->weather[0]->description);
+
+    return sprintf($string, $value);
+  }
+
+  public function getExpoFact() {
+    $this->timeNow = time();
+
+    $this->expoArgs = array(
+      'post_type' => array( 'exhibition' ),
+      'orderby' => 'meta_value_num',
+      'meta_key' => '_igv_exhibition_start',
+      'posts_per_page' => 1,
+      'tax_query' => array(
+        array(
+          'taxonomy' => 'location',
+          'field'    => 'slug',
+          'terms'    => 'galeria-agustina-ferreyra',
+        ),
+    	),
+    );
+
+    switch (array_rand($this->facts['expo'])) {
+      case 'closes':
+        return $this->getExpoCloses();
+        break;
+      case 'opens':
+        return $this->getExpoOpens();
+        break;
+      case 'closed':
+        return $this->getExpoClosed();
+        break;
+    }
+  }
+
+  public function getExpoCloses() {
+    // Display current exhibition fact
+
+    $this->expoArgs['meta_query'] = array(
+      'relation' => 'AND',
+      array(
+        'key'     => '_igv_exhibition_start',
+        'value'   => $this->timeNow,
+        'compare' => '<',
+      ),
+      array(
+        'key'     => '_igv_exhibition_end',
+        'value'   => $this->timeNow,
+        'compare' => '>',
+      ),
+    );
+
+    $expo_query = new WP_Query( $this->expoArgs );
+
+    // Display when current exhibition ends
+    if ($expo_query->have_posts()) {
+      while($expo_query->have_posts()) {
+        $expo_query->the_post();
+
+        $string = $this->facts['expo']['closes'];
+
+        $expo_end_date = get_post_meta(get_the_ID(), '_igv_exhibition_end', true);
+
+        $value = abs($this->timeNow - $expo_end_date)/60/60/24;
+
+        return sprintf($string, $value);
+      }
+    } else {
+      unset($this->facts['expo']['closes']);
+
+      return false;
+    }
+  }
+
+  public function getExpoOpens() {
+    // Display upcoming exhibition fact
+
+    $this->expoArgs['meta_query'] = array(
+      array(
+        'key'     => '_igv_exhibition_start',
+        'value'   => $this->timeNow,
+        'compare' => '>',
+      ),
+    );
+
+    $expo_query = new WP_Query( $this->expoArgs );
+
+    // Display when next exhibition starts
+    if ($expo_query->have_posts()) {
+      while($expo_query->have_posts()) {
+        $expo_query->the_post();
+
+        $string = $this->facts['expo']['opens'];
+
+        $expo_start_date = get_post_meta(get_the_ID(), '_igv_exhibition_start', true);
+
+        $value = abs($this->timeNow - $expo_start_date)/60/60/24;
+
+        return sprintf($string, $value);
+      }
+    } else {
+      unset($this->facts['expo']['opens']);
+
+      return false;
+    }
+  }
+
+  public function getExpoClosed() {
+    // Display past exhibition fact
+
+    $this->expoArgs['meta_query'] = array(
+      array(
+        'key'     => '_igv_exhibition_end',
+        'value'   => $this->timeNow,
+        'compare' => '<',
+      ),
+    );
+
+    $expo_query = new WP_Query( $this->expoArgs );
+
+    // Display when last exhibition ended
+    if ($expo_query->have_posts()) {
+      while($expo_query->have_posts()) {
+        $expo_query->the_post();
+
+        $string = $this->facts['expo']['closed'];
+
+        $expo_end_date = get_post_meta(get_the_ID(), '_igv_exhibition_end', true);
+
+        $value = abs($this->timeNow - $expo_end_date)/60/60/24;
+
+        return sprintf($string, $value);
+      }
+    } else {
+      unset($this->facts['expo']['closed']);
+
+      return false;
+    }
+  }
+
+  public function getStringFact() {
+    return $this->facts['string'][array_rand($this->facts['string'])];
+  }
 }
