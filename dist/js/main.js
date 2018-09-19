2935a85663a3e4a0099c3b6895a42929943a79ec
mikeyrayvon <mikey@interglobal.vision>
2018-09-19 17:52:50 -0500

    bind onReady

--- dist/js/main.js
+++ dist/js/main.js
@@ -6828,13 +6828,14 @@ var Mailchimp = function () {
 
     this.mobileThreshold = 601;
 
-    $(window).on('ajaxSuccess', this.onReady.bind(this)); // Bind ajaxSuccess (custom event, comes from Ajaxy)
-
-    $(document).ready(this.onReady.bind(this));
-
     // Bind functions
+    this.onReady = this.onReady.bind(this);
     this.submitForm = this.submitForm.bind(this);
     this.successMessage = this.successMessage.bind(this);
+
+    $(window).on('ajaxSuccess', this.onReady); // Bind ajaxSuccess (custom event, comes from Ajaxy)
+
+    $(document).ready(this.onReady);
   }
 
   _createClass(Mailchimp, [{
--- src/js/mailchimp.js
+++ src/js/mailchimp.js
@@ -5,13 +5,14 @@ class Mailchimp {
   constructor() {
     this.mobileThreshold = 601;
 
-    $(window).on('ajaxSuccess', this.onReady.bind(this)); // Bind ajaxSuccess (custom event, comes from Ajaxy)
-
-    $(document).ready(this.onReady.bind(this));
-
     // Bind functions
+    this.onReady = this.onReady.bind(this);
     this.submitForm = this.submitForm.bind(this);
     this.successMessage = this.successMessage.bind(this);
+
+    $(window).on('ajaxSuccess', this.onReady); // Bind ajaxSuccess (custom event, comes from Ajaxy)
+
+    $(document).ready(this.onReady);
   }
 
   onReady() {
