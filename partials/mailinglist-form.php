62c5359ce5d7f84840094cdcad351a93f760b33c
mikeyrayvon <mikey@interglobal.vision>
2018-09-19 17:49:49 -0500

    improve mailchimp form markup

--- partials/mailinglist-form.php
+++ partials/mailinglist-form.php
@@ -3,10 +3,13 @@ $options = get_site_option('_igv_site_options');
 
 if (!empty($options['_igv_mailchimp_url'])) {
 ?>
-<form id="mailchimp-form" class="grid-row" novalidate="true">
-  <input id="mailchimp-email" type="email" placeholder="EMAIL" name="EMAIL" class="item-s-8 item-l-6">
-  <button type="submit" class="button-side-margin">ENTER</button>
-  <div id="mailchimp-response" class="item-s-12 margin-top-tiny">&nbsp;</div>
+<form id="mailchimp-form" class="grid-row margin-top-small" novalidate="true">
+  <div class="grid-item item-s-12 margin-bottom-micro">Join our mailing list:</div>
+  <div class="grid-item item-s-12">
+    <input id="mailchimp-email" type="email" placeholder="Email" name="EMAIL">
+    <button id="mailchimp-submit" type="submit" class="font-bold u-pointer">Subscribe</button>
+  </div>
+  <div id="mailchimp-response" class="grid-item item-s-12 margin-top-micro">&nbsp;</div>
 </form>
 <?php
 }
