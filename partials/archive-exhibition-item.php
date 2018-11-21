f71ac123ef47ba7624f3b15dd621932eacaf5d4e
mikeyrayvon <mikey@interglobal.vision>
2018-11-21 12:29:58 -0600

    emphasize title on single exhibitino page

--- partials/archive-exhibition-item.php
+++ partials/archive-exhibition-item.php
@@ -4,13 +4,13 @@ $title = get_post_meta($post->ID, '_igv_exhibition_title', true);
 $start = get_post_meta($post->ID, '_igv_exhibition_start', true);
 $end = get_post_meta($post->ID, '_igv_exhibition_end', true);
 $location = wp_get_post_terms($post->ID, 'location');
-$show_title = get_post_meta($post->ID, '_igv_exhibition_show_title', true);
+$emphasize_title = get_post_meta($post->ID, '_igv_exhibition_emphasize_title', true);
 ?>
 <article <?php post_class('grid-item item-s-12 grid-row no-gutter hover-dot margin-bottom-small'); ?> id="post-<?php the_ID(); ?>">
   <a href="<?php the_permalink() ?>" class="grid-item item-s-12 grid-row no-gutter">
     <div class="grid-item item-s-12 item-m-6 font-uppercase margin-bottom-micro">
     <?php
-      if (!empty($show_title) && !empty($title)) {
+      if (!empty($emphasize_title) && !empty($title)) {
         echo '<div class="font-uppercase font-size-large font-heavy">' . $title . '</div>';
       } else if (!empty($artists)) {
         foreach ($artists as $artist) {
